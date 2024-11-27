9360c6d812d3ec5a13ce0f56d6c66bac82c5f6fe
Dan Schalow <ds@danschalow.com>
2024-11-27 17:50:42 +0100

    refactor: pixel tracking function, to be called from tracking provider in varify.js

--- extensions/web-pixel/src/index.js
+++ extensions/web-pixel/src/index.js
@@ -2,48 +2,21 @@ import {register} from "@shopify/web-pixels-extension";
 
 register(({analytics, browser, init, settings}) => {
 
-  analytics.subscribe('varifyLocalStorage', async (event) => {
-    let variationData = {};
-    const {len: totalKeys, account_id: accountId} = event.customData;
+  analytics.subscribe('varifyStorage', async (event) => {
+    const {
+      experimentId,
+      variationId,
+      teamId,
+      storageType
+    } = event.customData;
 
-    for (let index = 0; index < totalKeys; index++) {
-      const key = await browser.localStorage.key(index);
+    const storageData = JSON.parse(browser[storageType].getItem('varify-data')) ?? {};
 
-      if (key.startsWith('varify-experiment-')) {
-        const value = await browser.localStorage.getItem(key);
-        const {variationId} = JSON.parse(value);
-        const experimentKey = key.replace('varify-experiment-', '');
-        variationData[experimentKey] = variationId;
-      }
-    }
+    storageData[experimentId] = variationId;
 
-    await browser.localStorage.setItem('varify', accountId);
+    await browser[storageType].setItem('varify', teamId);
 
-    if (Object.keys(variationData).length !== 0) {
-      await browser.localStorage.setItem('varify-data', JSON.stringify(variationData));
-    }
-  });
-
-  analytics.subscribe('varifySessionStorage', async (event) => {
-    let variationData = {};
-    const {len: totalKeys, account_id: accountId} = event.customData;
-
-    for (let index = 0; index < totalKeys; index++) {
-      const key = await browser.sessionStorage.key(index);
-
-      if (key.startsWith('varify-experiment-')) {
-        const value = await browser.sessionStorage.getItem(key);
-        const {variationId} = JSON.parse(value);
-        const experimentKey = key.replace('varify-experiment-', '');
-        variationData[experimentKey] = variationId;
-      }
-    }
-
-    await browser.localStorage.setItem('varify', accountId);
-
-    if (Object.keys(variationData).length !== 0) {
-      await browser.localStorage.setItem('varify-data', JSON.stringify(variationData));
-    }
+    await browser.localStorage.setItem('varify-data', JSON.stringify(storageData));
   });
 
   analytics.subscribe('checkout_completed', async (event) => {
